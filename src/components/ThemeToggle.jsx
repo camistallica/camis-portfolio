18abf132971b7c6fdace37226c5343a00f496593
camila ferreira <camilaferri108@gmail.com>
2025-12-01 01:07:46 -0300

    alternancia entre tema claro e escuro concluída

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react"
+import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
     const [temaEscuro, setTemaEscuro] = useState(false);
@@ -28,7 +29,9 @@ export const ThemeToggle = () => {
         }
     };
 
-    return <button onClick={toggleTheme}> {" "} 
+    return <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-hidden"
+    )}>
     {temaEscuro ? <Sun className="h-6 w-6 text-yellow-300"/> : <Moon className="h-6 w-6 text-blue-900"/>}
     </button>
 }
