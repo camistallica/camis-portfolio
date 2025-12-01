319fc231f3b17397f0a5ba816ccffc09fbbb0176
camila ferreira <camilaferri108@gmail.com>
2025-12-01 01:14:23 -0300

    criação do arquivo para o background de estrelas

---
+++ src/components/EstrelaBackground.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+//id, tamanho, x, y, opacidade, duracaoAnimacao
+
+export const EstrelaBackground = () => {
+    const [estrelas, setEstrelas] = useState([]);
+
+    const gerarEstrelas = () => {
+        
+    }
+    return <div> <div/>;
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import { ThemeToggle } from "../components/ThemeToggle"
+import { EstrelaBackground } from "@/components/EstrelaBackground"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* tema*/}
             <ThemeToggle/>
         {/* animações*/}
-
+            <EstrelaBackground/>
         {/* navbar*/}
 
         {/* principal*/}
